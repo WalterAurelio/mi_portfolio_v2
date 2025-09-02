7aeb19d4f42f2956f85be22eb0c2a330441edc0c
WalterAurelio <aureliogareca.dev@gmail.com>
2025-09-02 12:17:26 -0300

    Se definió la estructura para las secciones Hero, Sobre mí, Tecnologías y Proyectos.

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+function App() {
+  return (
+    <div className='font-overused-grotesk px-10 overflow-x-hidden'>
+
+      <section className='bg-red-200 h-dvh flex flex-col justify-between py-10'>
+        <div className='flex flex-col gap-10 bg-amber-200'>
+          <h1 className='text-[244.16px] uppercase leading-48'>Aurelio.</h1>
+          <p className='font-high-summit text-[61.04px]'>Frontend Developer</p>
+        </div>
+        <p className='text-xl w-[521px] bg-amber-500'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris non urna id nibh mattis vulputate at at velit.</p>
+      </section>
+
+      <section className='bg-green-300 h-dvh flex flex-col justify-between py-10'>
+        <div className='flex flex-col bg-amber-200 max-w-[1062px] relative'>
+          <h2 className='text-[97.66px]'>Sobre mí.</h2>
+          <p className='text-[25px]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris non urna id nibh mattis vulputate at at velit. Phasellus sed feugiat felis. Suspendisse tristique mollis lorem quis ullamcorper. Donec quis nunc consequat, lobortis odio at, ultrices nulla. Duis euismod elit vitae tincidunt faucibus.</p>
+          <p className='font-high-summit text-[61.04px] absolute left-[108px] top-18'>Sobre mi</p>
+        </div>
+      </section>
+
+      <section className='bg-red-200 h-dvh flex flex-col justify-between py-10'>
+        <div className='flex flex-col bg-amber-200 max-w-[534px] relative m-auto'>
+          <h2 className='text-[97.66px]'>Tecnologías.</h2>
+          <p className='text-xl text-center'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris non urna id nibh mattis vulputate at at velit. Phasellus sed feugiat felis.</p>
+          <p className='font-high-summit text-[61.04px] absolute left-[108px] top-18'>Tecnologias</p>
+        </div>
+      </section>
+
+      <section className='bg-green-300 h-dvh flex flex-col justify-between py-10'>
+        <div className='flex flex-col bg-amber-200 max-w-[1062px] relative'>
+          <h2 className='text-[183.12px] uppercase text-nowrap leading-none'>Proyectos • Proyectos • Proyectos • </h2>
+          <p className='text-[25px]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris non urna id nibh mattis vulputate at at velit. Phasellus sed feugiat felis. Suspendisse tristique mollis lorem quis ullamcorper. Donec quis nunc consequat, lobortis odio at, ultrices nulla. Duis euismod elit vitae tincidunt faucibus.</p>
+          <p className='font-high-summit text-[61.04px] absolute left-[108px] top-18'>Proyectos</p>
+        </div>
+      </section>
+
+    </div>
+  )
+}
+export default App
