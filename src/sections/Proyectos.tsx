2c48da1a34bc6e0187d717c15b745f7e599cf115
WalterAurelio <aureliogareca.dev@gmail.com>
2025-09-10 16:18:30 -0300

    Se implementaron animaciones para el título de la sección Proyectos.

--- src/sections/Proyectos.tsx
+++ src/sections/Proyectos.tsx
@@ -2,7 +2,7 @@ function Proyectos() {
   return (
     <section className='bg-green-300 h-dvh flex flex-col justify-between py-10'>
       <div className='flex flex-col gap-10 bg-amber-200 max-w-[1062px] relative'>
-        <h2 className='text-[183.12px] uppercase text-nowrap leading-none z-1'>Proyectos • Proyectos • Proyectos • </h2>
+        <h2 className='text-[183.12px] uppercase text-nowrap leading-none z-1 proyectos-text w-fit'>Proyectos • Proyectos • Proyectos • Proyectos • </h2>
         <p className='text-[25px]'>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris non urna id nibh mattis vulputate at at velit. Phasellus sed feugiat felis. Suspendisse tristique mollis
           lorem quis ullamcorper. Donec quis nunc consequat, lobortis odio at, ultrices nulla. Duis euismod elit vitae tincidunt faucibus.
